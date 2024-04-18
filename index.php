35660c0a5edbdf8676ca08ae65048ad27e6e3444
nicodellolio <151004233+nicodellolio@users.noreply.github.com>
2024-04-18 14:44:55 +0200

    add class var properties + constructor + method

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
 <?php 
 
+class Production {
+    // public string $title;
+    // public string $language;
+    // public int $rating;
+
+    function __construct(public string $title, public string $language, public int $rating ){
+
+        $this->title = $title;
+        $this->language = $language;
+        $this->rating = $rating;
+    }
+
+    /* public */ function isNowOut($language) {
+        return "$this->title is now available in $language. You better rush, because it's been rated $this->rating";
+    }
+}
+
+
+
 
 ?>
